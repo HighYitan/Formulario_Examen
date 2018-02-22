89b65545dd79e355ea235090df05d529af4c7ee7
HighYitan <32370248+HighYitan@users.noreply.github.com>
2018-02-22 20:52:38 +0100

    Add files via upload

--- js/Formulario.js
+++ js/Formulario.js
@@ -1,5 +1,9 @@
 window.onload = function () {
 	var url = "https://rawgit.com/HighYitan/Formulario_Examen/master/json/Formulario.json";
+	var formElement=null;
+	var respuestasTexto=[];
+	var respuestaSelect=null;
+	
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -10,6 +14,22 @@ window.onload = function () {
 	xhttp.open("GET", url, true); //Abre la url del JSON
 	xhttp.send();
 
+	// función personalizada que gestiona la respuesta a la petición de fichero.
+	
+	function gestionarXml(datosXml){
+		//Recuperamos el título y la respuesta correcta de Input, guardamos el número secreto
+		var xmlDoc = datosXml.responseXML;
+		imprimirTitulos(xmlDoc);
+		//preguntasTexto
+		for(var answers=2 ; answers<4 ; answers++){
+			respuestasTexto[answers]=xmlDoc.getElementsByTagName("answer")[answers].innerHTML;
+	}
+	
+	
+	
+	
+	
+	
 		var obj = JSON.parse(dadesJson);
 	
 	parser = new DOMParser();
